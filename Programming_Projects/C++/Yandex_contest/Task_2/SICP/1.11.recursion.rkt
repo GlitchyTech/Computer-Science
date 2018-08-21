35cd457d2db35a7331ea436e5f0d8a1fca99b2c6
Vasya Evdokimov <gertab95@gmail.com>
2018-08-21 15:34:09 +0300

    Updated

--- Programming_Projects/C++/Yandex_contest/Task_2/SICP/1.11.recursion.rkt
+++
@@ -1,8 +0,0 @@
-#lang racket
-(define (function n)
-  (cond ((< n 3) n)
-        (else (+ (function (- n 1))
-                 (function (- n 2))
-                 (function (- n 3))))))
-
-(function 5)
